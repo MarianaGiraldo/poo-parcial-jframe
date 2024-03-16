018ce3a000dea6fb51828c53222de5c400894396
Alejandro Prieto <159260161+Alejandro-Prieto-UN@users.noreply.github.com>
2024-03-16 16:43:29 -0500

    Add files via upload
    La tabla que yo hice

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+package poo.main;
+
+
+public class Main {
+   
+
+ public static void main(String[] args) {
+     
+     Ventana wind= new Ventana();
+     
+    // wind.setVisible(true);
+     
+    // wind.setResizable(false);
+     
+   
+   
+    }
+}
